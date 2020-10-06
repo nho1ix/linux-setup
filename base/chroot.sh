d6cef50bdd276e14b25663b40de35f323a723dd5
nho1ix <tupilbewb@gmail.com>
2020-10-06 09:28:05 -0700

    Get rid of descriptive 'echo' lines because you can't see them anyway

--- base/chroot.sh
+++ base/chroot.sh
@@ -2,11 +2,9 @@
 #| Configuration in chroot |
 #+-------------------------+
 # Essential packages
-echo 'Downloading other packages'
 pacman --noconfirm -S $ucode networkmanager git vifm
 
 # Swap
-echo 'Making swap file if requested'
 if [ -n "$swap_size" ]; then
      dd if=/dev/zero of=/swapfile bs=1GiB count=$swap_size status=progress
      chmod 600 /swapfile
@@ -17,7 +15,6 @@ if [ -n "$swap_size" ]; then
 fi
 
 # Timezone and localization
-echo 'Setting timezone...'
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen && locale-gen
@@ -28,11 +25,9 @@ echo "::1             localhost" >> /etc/hosts
 echo "127.0.1.1       "$hostname".localdomain       "$hostname"" >> /etc/hosts
 
 # Network
-echo 'Enabling network functionality'
 systemctl enable NetworkManager
 
 # Boot
-echo 'Setting up bootloader..'
 if [ "$bootloader" = "systemd-boot" ]; then
      bootctl --path=/boot install
      echo 'default arch.conf' > /boot/loader/loader.conf
@@ -50,7 +45,6 @@ elif [ "$bootloader" = "efistub" ]; then
 fi
 
 # Silent boot
-echo 'Adding systemd hooks + lz4 compression'
 sed -i 's/^HOOKS=(base udev/HOOKS=(base systemd/g' /etc/mkinitcpio.conf
 sed -i 's/modconf block/modconf block sd-encrypt/g' /etc/mkinitcpio.conf
 sed -i 's/^#COMPRESSION="lz4"/COMPRESSION="lz4"/g' /etc/mkinitcpio.conf
@@ -60,18 +54,15 @@ mkinitcpio -p linux
 echo "StandardOutput=null\nStandardError=journal+console" | SYSTEMD_EDITOR="tee -a" systemctl edit --full systemd-fsck-root.service
 
 # Pacman
-echo 'Applying color to pacman...'
 sed -i 's/^#Color/Color/g;/#\[multilib\]/,/#Include/ s/^#//g' /etc/pacman.conf
 sed -i '33 a\ILoveCandy' /etc/pacman.conf
 pacman -Syy
 
 # Makepkg
-echo 'Enabling parallel compilation and compression.."
 sed -i 's/-march=x86_64 -mtune=generic/-march=native/g' /etc/makepkg.conf
 sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
 
 # Users
-echo "Please enter the root password for this computer:"
 passwd
 useradd -m -G wheel -s /bin/zsh -c "$fullname" "$username"
 echo "Please enter the password for "$username":"
@@ -81,12 +72,10 @@ passwd "$username"
 sed -i '/%wheel\sALL=(ALL)\sNOPASSWD:\sALL/s/^#\s//g' /etc/sudoers
 
 # Change shell to zsh
-echo 'Changing shell for user'
 chsh -s /bin/zsh
 chsh -s /bin/zsh victor
 
 # Manual intervention
-echo 'Symlinking vi to vim (visudo)'
 ln -s /usr/bin/vim /usr/bin/vi
 vim /boot/loader/entries/arch.conf
 
@@ -98,5 +87,5 @@ if [[ ! -f afterinstall.sh ]]; then
 fi
 chmod +x afterinstall.sh
 rm /chroot.sh
-echo 'The installation has finished. VARBs will be in your user's home directory. Please run the VARBs script after rebooting. I will exit now'
+echo 'The installation has finished. I will exit now'
 exit
