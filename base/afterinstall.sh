fe1586bedd5462c8890b30fbbf435ac8dadcd24b
nho1ix <tupilbewb@gmail.com>
2020-10-06 09:26:29 -0700

    Fix script

--- base/afterinstall.sh
+++ base/afterinstall.sh
@@ -3,12 +3,13 @@
 # Clone personal dotfiles repo & yay
 git clone https://github.com/nho1ix/dotfiles.git
 mv dotfiles/* $HOME
-# git clone https://aur.archlinux.org/yay.git
+mv -i dotfiles/.* $HOME
 
 # Compile yay
-# cd yay
-# makepkg -si
-# cd && rm -rf yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd && rm -rf yay
 
 # Clone suckless utilities
 git clone https://github.com/nho1ix/dwm.git
@@ -16,12 +17,15 @@ git clone https://github.com/nho1ix/dwmblocks.git
 git clone https://github.com/nho1ix/dmenu.git
 git clone https://github.com/nho1ix/st.git
 git clone https://github.com/nho1ix/bdata.git
-mv bdata/.config $HOME && mv bdata/.mozilla $HOME
+
+# Merge bdata files with home directory
+mv -n bdata/.config $HOME && mv -n bdata/.mozilla $HOME
+
 # Make suckless directory and move files to directory
 mkdir .config/suckless && mv dwm dwmblocks dmenu st .config/suckless/
 
 # Install AUR packages
-# yay -S ifuse brave-bin alsa-utils usbutils ttf-font-awesome ytop ios-emoji zathura zathura-pdf-poppler picom-ibhagwan-git instagram-nativefier libxft-bgra-git paper-icon-theme-git ripgrep udiskie vifmimg-git vim-lightline-git vim-ultisnips vim-vimwiki vundle-git xclip --noconfirm
+yay -S ifuse brave-bin alsa-utils usbutils ttf-font-awesome ytop ios-emoji zathura zathura-pdf-poppler picom-ibhagwan-git instagram-nativefier libxft-bgra-git paper-icon-theme-git ripgrep udiskie vifmimg-git vim-lightline-git vim-ultisnips vim-vimwiki vundle-git xclip --noconfirm
 
 # Install plugins for vim
 vim -c PluginInstall &
